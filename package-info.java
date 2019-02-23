668b508ec818770df29e6a984b86e39a9cdcdb6c
yaitox <47457721+yaitox@users.noreply.github.com>
2019-02-23 10:54:37 -0500

    Update package-info.java
    A basic use of control structures applied on ATM code... What y'all think?

--- package-info.java
+++ package-info.java
@@ -14,23 +14,43 @@ class Main {
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		// SABER SI UN AÑO ES BISIESTO O NO
+		// ATM
 
-		System.out.println("INGRESE UN RANGO DE AÑOS");
-		int anio;
-		anio = scanner.nextInt();
+		System.out.println("Welcome to the system!");
 
-		if (anio % 4 == 0)
-			if (anio % 100 == 0)
-				if (anio % 400 == 0)
-					System.out.println("es bisiesto");
+		 int value, dollar_Hundred, cash_twentyThousand, billetes_diezMil, residue50000, residue20000, residue10000;
 
-				else
-					System.out.println("no es bisiesto");
-			else
-				System.out.println("es bisiesto");
-		else
-			System.out.println("no es bisiesto");
+		 System.out.println("Please digit a value that you want\nthe value has to be greater than 10 and smaller than 240 dollars\nand has to be multiple of 10");
+		 valor = scanner.nextInt();
+		 if(value<10)
+		  System.out.println("The value has to be greater than 10!");
+		 if(value<10)
+		    System.exit( 0 );
+		 
+		 
+		 {
+		  if (value % 10 != 0)
+		   System.out.println("Only multiples of 10!");
+		  if (value % 10 != 0)
+		   System.exit(0);
+		 }
+		 {
+		  dollar_Hundred = value / 100;
+		  residue100 = valor % 100;
 
+		  dollar_Fifty = residue100 / 50;
+		  residue50 = residue100 % 20000;
+
+		  dollar_Ten = residue50 / 10;
+		  residue10 = residue50 % 10;
+
+		  System.out.println("you get " + dollar_Hundred + " dollars of 100 value");
+		  System.out.println("you get " + dollar_Fifty + " dollars of 50 value");
+		  System.out.println("you get " + dollar_Ten + " dollars of 10 value");
+		 
+		 }
+
+		}
+
+		
 	}
-}
